fbbd9ffd761a7a55d14afdb5f509120aa0d4f5e5
linat21 <leatheberner@gmail.com>
2022-08-24 11:17:30 +0300

    Add urls and view-functions

--- sNetwork/posts/views.py
+++ sNetwork/posts/views.py
@@ -5,6 +5,5 @@ def index(request):
     return HttpResponse('Это главная страница')
 
 
-
-def group_posts(response):
+def group_posts(request, slug):
 	return HttpResponse('Здесь будут посты, отфильтрованные по группам')
