85667a85732a926890b92bc0d2cfd7ab9b829fa5
linat21 <leatheberner@gmail.com>
2022-08-25 21:28:56 +0300

    Add template

--- sNetwork/posts/views.py
+++ sNetwork/posts/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Это главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
