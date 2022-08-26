4b4c1e5d958dd87629a8937a3cc08cf74efae65f
linat21 <leatheberner@gmail.com>
2022-08-26 14:36:21 +0300

    Add context dictionary

--- sNetwork/posts/views.py
+++ sNetwork/posts/views.py
@@ -1,11 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта sNetwork'
+    context = {
+        'text': text
+        }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Здесь будет информация о группах проекта sNetwork'
+    context = {
+        'text': text
+        }
+    return render(request, template, context)
