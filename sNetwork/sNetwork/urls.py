e8636f087373168b25d5fbcde571cf5895742cc0
linat21 <leatheberner@gmail.com>
2022-09-15 21:33:21 +0300

    Models & migrations

--- sNetwork/sNetwork/urls.py
+++ sNetwork/sNetwork/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts')),
+    path('', include('posts.urls', namespace='main')),
     path('group/<slug:slug>/', include('posts.urls', namespace='posts'))
 ]
