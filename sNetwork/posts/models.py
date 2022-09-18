c3a2c3eb89295203b7f6a1c3ba95de8b5a6b4590
linat21 <leatheberner@gmail.com>
2022-09-18 21:15:43 +0300

    Add search interface (without check) & groups with links

--- sNetwork/posts/models.py
+++ sNetwork/posts/models.py
@@ -6,9 +6,12 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    pub_date = models.DateTimeField("Published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey('Group', on_delete=models.SET_NULL, blank=True, null=True)
+    group = models.ForeignKey('Group', on_delete=models.SET_NULL, blank=True, null=True, related_name="group_posts")
+
+    def __str__(self):
+        return self.text
 
 
 class Group(models.Model):
@@ -16,3 +19,5 @@ class Group(models.Model):
     slug = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title[:10]
--- sNetwork/posts/views.py
+++ sNetwork/posts/views.py
@@ -1,14 +1,31 @@
+import datetime
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')
     context = {
         'posts': posts
         }
     return render(request, template, context)
+
+# def index(request):
+#     author = User.objects.get(username='leo')
+#     keyword = "утро"
+#     start_date = datetime.datetime(1854, 7, 7, 0, 0, tzinfo=datetime.timezone.utc)
+#     end_date = datetime.datetime(1854, 7, 21, 0, 0, tzinfo=datetime.timezone.utc)
+#     posts = Post.objects.filter(text__contains=keyword).filter(author=author).filter(pub_date__range=(start_date, end_date))
+#     return render(request, 'posts/index.html', {'posts': posts})
+
+# def index(request):
+#     keyword = request.GET.get('q', None)
+#     if keyword:
+#         posts = Post.objects.select_related('author', 'group').filter(text__contains=keyword)
+#     else:
+#         posts = None
+#     return render(request, 'posts/index.html', {'posts': posts, 'keyword': keyword})
 
 
 def groups(request):
